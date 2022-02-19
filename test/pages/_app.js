afc86d5f6e32e065763513643a417989aa6c6d44
andrescimma1 <andrescimma1@gmail.com>
2022-02-19 15:32:49 -0300

    more styles

--- test/pages/_app.js
+++ test/pages/_app.js
@@ -1,7 +1,14 @@
 import "../styles/globals.css";
+import { AgaveContainer } from "@x99minutos/agave-material";
+import "@x99minutos/agave-material/dist/index.css";
+import { Typography } from "@mui/material";
 
 function MyApp({ Component, pageProps }) {
-	return <Component {...pageProps} />;
+	return (
+		<AgaveContainer>
+			<Component {...pageProps} />
+		</AgaveContainer>
+	);
 }
 
 export default MyApp;
